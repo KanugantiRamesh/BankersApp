41b85ab26b5b43e4831875c85a949c1784dd1802
kothurisuresh <ksuresh@thrymr.net>
2015-09-23 19:22:21 +0530

    LeaderBoard

--- app/src/main/java/thrymr/apps/materialtests/MainActivity.java
+++ app/src/main/java/thrymr/apps/materialtests/MainActivity.java
@@ -1,11 +1,16 @@
 package thrymr.apps.materialtests;
 
 import android.os.Bundle;
+import android.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.widget.Toast;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+import thrymr.apps.materialtests.models.DailyChallenge;
 
 
 public class MainActivity extends ActionBarActivity implements NavigationDrawerCallbacks {
@@ -20,7 +25,8 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerC
         mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-
+        ParseObject.registerSubclass(DailyChallenge.class);
+        Parse.initialize(this, "5QPOyyZjjWf0xyWXpBfwf6bNljAYzu9wmGsti1DN", "lkaXuewH6QYMXcqovcmw9Dr3BJ5ghMpJ7MnFCK1x");
 
 
         mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager().findFragmentById(R.id.fragment_drawer);
@@ -35,7 +41,41 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerC
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
-        Toast.makeText(this, "Menu item selected -> " + position, Toast.LENGTH_SHORT).show();
+        Fragment newfrag = null;
+        switch (position) {
+            case 0:
+                break;
+            case 1:
+			/*
+			 * final Fragment newfrag1 = new AskDieticianFragment();
+			 * HomeActivity.this.startNewFragment(newfrag1, "newfrag");
+			 */
+                break;
+            case 2:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.container, new LeaderBoard(),
+                                "NavBackStack0").setTransition(4099).commit();
+
+                break;
+            case 3:
+                // newfrag = new WeightTrackerAnalysis();
+                break;
+            case 4:
+
+                break;
+
+            case 5:
+                break;
+            case 6:
+                break;
+
+            case 7:
+                break;
+
+            default:
+                break;
+        }
     }
 
     @Override
---
+++ app/src/main/java/thrymr/apps/materialtests/models/DailyChallenge.java
@@ -0,0 +1,33 @@
+package thrymr.apps.materialtests.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+/**
+ * Created by thrymr on 23/9/15.
+ */
+@ParseClassName("DailyChallenge")
+public class DailyChallenge extends ParseObject {
+
+
+    public String getUserName() {
+        return getString("user_name");
+    }
+
+    public void setUserName(String name) {
+        put("user_name", name);
+    }
+
+    public Number getUserScoredPoints() {
+        return getNumber("points_scored");
+    }
+
+    public void setUserScoredPoints(Number scoredPoints) {
+        put("points_scored", scoredPoints);
+    }
+
+    @Override
+    public String toString() {
+        return getString("user_name")+"\n"+getNumber("points_scored");
+    }
+}
